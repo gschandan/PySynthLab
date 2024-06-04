4bf31978c0b066f8e3b45dd7f6bd976cccbb5b4a
Gurmukh Chandan <gsc128@gmail.com>
2024-06-04 20:23:49 +0100

    fixing tests

--- tests/given_constraints_to_substitute.py
+++ tests/given_constraints_to_substitute.py
@@ -5,13 +5,21 @@ from pysynthlab.cegis.z3.synthesis_problem_z3 import SynthesisProblemOptions, Sy
 
 class WhenTheConstraintIsCommutativity(unittest.TestCase):
     def setUp(self):
+        # self.problem_str = """
+        #     (set-logic LIA)
+        #     (synth-fun f ((x Int) (y Int)) Int)
+        #     (declare-var x Int)
+        #     (declare-var y Int)
+        #     (constraint (= (f x y) (f y x)))
+        #     (constraint (and (<= x (f x y)) (<= y (f x y))))
+        #     (check-synth)
+        #     """
         self.problem_str = """
             (set-logic LIA)
             (synth-fun f ((x Int) (y Int)) Int)
             (declare-var x Int)
             (declare-var y Int)
             (constraint (= (f x y) (f y x)))
-            (constraint (and (<= x (f x y)) (<= y (f x y))))
             (check-synth)
             """
         self.options = SynthesisProblemOptions()
@@ -22,57 +30,98 @@ class WhenTheConstraintIsCommutativity(unittest.TestCase):
         self.args = [self.problem.context.z3_variables["x"], self.problem.context.z3_variables["y"]]
 
     def test_substitute_constraints_commutativity_with_invalid_function(self):
-        candidate_expr, _ = self.problem.generate_non_commutative_solution(self.args)
+        candidate_expr, _ = self.problem.generate_invalid_solution_two(self.args)
         substituted_constraints = self.problem.substitute_constraints(self.problem.context.z3_constraints, self.func, candidate_expr)
 
         self.assertGreater(len(substituted_constraints), 0)
 
+        expected_commutativity = substitute(
+            And(self.func(self.args[0], self.args[1]) == self.func(self.args[1], self.args[0])),
+            (self.func(self.args[0], self.args[1]), candidate_expr(*self.args)),
+            (self.func(self.args[1], self.args[0]), candidate_expr(*self.args))
+        )
+
         for constraint in substituted_constraints:
             self.assertIsInstance(constraint, BoolRef)
             self.assertIn("If", str(constraint))
-            self.assertIn("<=", str(constraint))
+            #self.assertIn("<=", str(constraint))
 
-        # Check that the commutativity constraint has been correctly enforced
-        commutativity_constraints = [str(c) for c in substituted_constraints if "==" in str(c)]
+        commutativity_constraints = [c for c in substituted_constraints if "==" in str(c)]
         self.assertGreater(len(commutativity_constraints), 0)
+        self.assertIn(str(expected_commutativity), [str(c) for c in commutativity_constraints])
 
-        for commutativity_constraint in commutativity_constraints:
-            self.assertIn("x", commutativity_constraint)
-            self.assertIn("y", commutativity_constraint)
-            self.assertIn("==", commutativity_constraint)
-            self.assertNotEqual(commutativity_constraint.count("If"), 1)
+        solver = Solver()
+        solver.add(substituted_constraints)
+        self.assertNotEqual(solver.check(), sat)
 
-        # Check that the non-commutative function does not satisfy commutativity
-        for constraint in substituted_constraints:
-            self.assertNotEqual(str(constraint), "If(x > y, x, y - 1) == If(y > x, y, x - 1)")
-
-    def test_substitute_constraints_commutativity(self):
-
-        candidate_expr, _ = self.problem.generate_max_function(self.args)
-        substituted_constraints = self.problem.substitute_constraints(
-            self.problem.context.z3_constraints,
-            self.func,
-            candidate_expr
-        )
+    def test_substitute_constraints_commutativity_with_abs_max_function(self):
+        candidate_expr, _ = self.problem.generate_correct_abs_max_function(self.args)
+        substituted_constraints = self.problem.substitute_constraints(self.problem.context.z3_constraints, self.func, candidate_expr)
 
         self.assertGreater(len(substituted_constraints), 0)
 
-        commutativity_constraint = substituted_constraints[0]
-        self.assertIsInstance(commutativity_constraint, BoolRef)
-        self.assertIn("==", str(commutativity_constraint))
+        expected_commutativity = substitute(
+            And(self.func(self.args[0], self.args[1]) == self.func(self.args[1], self.args[0])),
+            (self.func(self.args[0], self.args[1]), candidate_expr(*self.args)),
+            (self.func(self.args[1], self.args[0]), candidate_expr(*self.args))
+        )
 
-        other_constraints = substituted_constraints[1:]
-        for constraint in other_constraints:
+        for constraint in substituted_constraints:
             self.assertIsInstance(constraint, BoolRef)
-            self.assertIn("<=", str(constraint))
-            self.assertIn("If", str(constraint))
-            self.assertIn("x", str(constraint))
-            self.assertIn("y", str(constraint))
+
+        commutativity_constraints = [c for c in substituted_constraints if "==" in str(c)]
+        self.assertGreater(len(commutativity_constraints), 0)
+        self.assertIn(str(expected_commutativity), [str(c) for c in commutativity_constraints])
 
         solver = Solver()
         solver.add(substituted_constraints)
         self.assertEqual(solver.check(), sat)
 
+    def test_substitute_constraints_commutativity_with_integer(self):
+        candidate_expr = z3.IntVal(1)
+        substituted_constraints = self.problem.substitute_constraints(self.problem.context.z3_constraints, self.func, candidate_expr)
+
+        self.assertGreater(len(substituted_constraints), 0)
+
+        expected_commutativity = substitute(
+            And(self.func(self.args[0], self.args[1]) == self.func(self.args[1], self.args[0])),
+            (self.func(self.args[0], self.args[1]), candidate_expr),
+            (self.func(self.args[1], self.args[0]), candidate_expr)
+        )
+
+        for constraint in substituted_constraints:
+            self.assertIsInstance(constraint, BoolRef)
+
+        commutativity_constraints = [c for c in substituted_constraints if "==" in str(c)]
+        self.assertGreater(len(commutativity_constraints), 0)
+        self.assertIn(str(expected_commutativity), [str(c) for c in commutativity_constraints])
+
+        solver = Solver()
+        solver.add(substituted_constraints)
+        self.assertEqual(solver.check(), sat)
+
+    def test_substitute_constraints_commutativity_with_arithmetic_expression(self):
+        candidate_expr, _ = self.problem.generate_arithmetic_function(self.args, depth=2, complexity=1)
+        substituted_constraints = self.problem.substitute_constraints(self.problem.context.z3_constraints, self.func, candidate_expr)
+
+        self.assertGreater(len(substituted_constraints), 0)
+
+        expected_commutativity = substitute(
+            And(self.func(self.args[0], self.args[1]) == self.func(self.args[1], self.args[0])),
+            (self.func(self.args[0], self.args[1]), candidate_expr(*self.args)),
+            (self.func(self.args[1], self.args[0]), candidate_expr(*self.args))
+        )
+
+        for constraint in substituted_constraints:
+            self.assertIsInstance(constraint, BoolRef)
+
+        commutativity_constraints = [c for c in substituted_constraints if "==" in str(c)]
+        self.assertGreater(len(commutativity_constraints), 0)
+        self.assertIn(str(expected_commutativity), [str(c) for c in commutativity_constraints])
+
+        solver = Solver()
+        solver.add(substituted_constraints)
+        self.assertEqual(solver.check(), sat)
 
 if __name__ == "__main__":
     unittest.main()
